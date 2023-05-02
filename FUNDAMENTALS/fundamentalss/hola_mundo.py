6fcb938fbd99a55ce0bbc4a05ad3da72dbce98bd
danae21 <danaedelgado21@gmail.com>
2023-05-02 15:50:19 -0400

    first commit

---
+++ FUNDAMENTALS/fundamentalss/hola_mundo.py
@@ -0,0 +1,24 @@
+# 1. TAREA: imprime "Hola, mundo"
+print("Hola mundo saluda Danae")
+
+
+# 2. imprime "Hola, Noelle" con el nombre en una variable
+name = "Noelle"
+print( tu código aquí )	# con una coma
+print( tu código aquí )	# con un +
+
+
+
+# 3. imprimir "Hola 42!" con el número en una variable
+name = 42
+print( tu código aquí )	# con una coma
+print( tu código aquí )	# con una +	-- este debería arrojar un error!
+
+
+
+# 4. imprimir "Amo comer sushi y pizza" con las comidas en variables
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print( tu código aquí ) # con .format()
+print( tu código aquí ) # con una cadena f
+
---
+++ ej.py
@@ -0,0 +1,17 @@
+
+for x in range(0,151):
+    print(x)
+
+
+for x in range (0,1005,5):
+    print(x)
+
+
+for val in range(0,101):
+    if val %5 == 0:
+        print('dojo')
+        print(val)
+    if val %10 ==0:
+        print('coding dojo')
+        print(val)
+
